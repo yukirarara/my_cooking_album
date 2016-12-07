c90aeef8e83a9d053603d514ba782ce742a682a1
yukiriru <k_ootake@outlook.jp>
2016-12-07 19:37:31 +0000

    レシピテーブルと材料のマスターテーブルの中間テーブル

---
+++ cook_album/database/migrations/2016_12_05_051113_create_recipe_ms_ingredients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipeMsIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('recipe_ms_ingredients')) {
+            Schema::create('recipe_ms_ingredients', function(Blueprint $table) {
+                $table->increments('id');
+                # TODO: resipe_idとms_ingredients_idのdefaultはとりあえず。
+                $table->integer('recipe_id')->default(1);
+                $table->integer('ms_ingredients_id')->default(1);
+                $table->string('new_ingredients_name')->default('');
+                $table->string('quantity')->default('');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Shema::dropifExists('recipe_ms_ingredients');
+    }
+}
